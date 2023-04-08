ba8300f35e93bf833b8129e943a379fcc110ee7c
arkii1 <tommy.mapp@hotmail.com>
2023-04-08 16:20:16 +0100

    Change carousel data

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,29 +4,9 @@ import Hero from '../sections/Hero'
 import Carousel from '../sections/Carousel'
 import Tech from '../sections/Tech'
 import theLocNar from '../../assets/images/the-loc-nar.jpg'
-import twitterSS from '../../assets/images/twitter.png'
-import websiteSS from '../../assets/images/website.png'
 
 function Home() {
   const data = [
-    {
-      title: 'Personal Website',
-      img: websiteSS,
-      black: true,
-      tech: ['JavaScript', 'React'],
-      description:
-        'A client-side website I developed to experiment with my design skills, and showcase my skills.',
-      link: 'personal-website',
-    },
-    {
-      title: 'Twitter Clone',
-      img: twitterSS,
-      black: true,
-      tech: ['JavaScript', 'React', 'Firebase'],
-      description:
-        'A clone of twitter including CRUD operations and Google Authentication using Firebase.',
-      link: 'twitter-clone',
-    },
     {
       // eslint-disable-next-line quotes
       title: "Where's Waldo",
