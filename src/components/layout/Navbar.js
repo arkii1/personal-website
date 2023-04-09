fd2dd19d05b7147e9fe50ca56943dbe1ff14450f
arkii1 <tommy.mapp@hotmail.com>
2023-04-09 13:29:45 +0100

    Change styles

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
 
   return (
     <animated.nav
-      className="bg-sky-500 text-white fixed shadow-xl w-screen bg-white flex justify-between px-4 py-2 gap-2 md:px-6 md:py-3 md:gap-3 md:justify-end lg:py-4 lg:px-8 lg:gap-5"
+      className="bg-white fixed shadow-xl w-screen bg-white flex justify-between px-4 py-2 gap-2 md:px-6 md:py-3 md:gap-3 md:justify-end lg:py-4 lg:px-8 lg:gap-5"
       style={{ zIndex: '999', ...fromUpSpring }}
     >
       <button type="button" onClick={scrollToHome}>
