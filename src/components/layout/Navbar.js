87ea5346c619fa4da4395eb9afecde6d416a18b3
arkii1 <tommy.mapp@hotmail.com>
2023-04-09 13:23:29 +0100

    Modify to scroll to home/contact

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,16 +6,29 @@ import { fromUp } from '../../utility/springConfigs'
 
 function Navbar() {
   const fromUpSpring = useSpring(fromUp)
+
+  const scrollToHome = () => {
+    const element = document.getElementById('home')
+    element.scrollIntoView({ behavior: 'smooth' })
+  }
+
+  const scrollToContact = () => {
+    const element = document.getElementById('contact')
+    element.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <animated.nav
-      className="fixed shadow-xl w-screen bg-white flex justify-between px-4 py-2 gap-2 md:px-6 md:py-3 md:gap-3 md:justify-end lg:py-4 lg:px-8 lg:gap-5"
+      className="bg-sky-500 text-white fixed shadow-xl w-screen bg-white flex justify-between px-4 py-2 gap-2 md:px-6 md:py-3 md:gap-3 md:justify-end lg:py-4 lg:px-8 lg:gap-5"
       style={{ zIndex: '999', ...fromUpSpring }}
     >
-      <Link to="/"> Home </Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/portfolio">Portfolio</Link>
+      <button type="button" onClick={scrollToHome}>
+        Home
+      </button>
       <Link to="/cv">CV</Link>
-      <Link to="/contact">Contact</Link>
+      <button type="button" onClick={scrollToContact}>
+        Contact
+      </button>
     </animated.nav>
   )
 }
