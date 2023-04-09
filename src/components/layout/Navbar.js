a3cb97f5b6152e1f7bb518f9ec843984683d81a3
arkii1 <tommy.mapp@hotmail.com>
2023-04-09 14:40:19 +0100

    Change styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './styles.css'
-import { Link } from 'react-router-dom'
 import { useSpring, animated } from 'react-spring'
 import { fromUp } from '../../utility/springConfigs'
 
@@ -22,11 +21,18 @@ function Navbar() {
       className="bg-white fixed shadow-xl w-screen bg-white flex justify-between px-4 py-2 gap-2 md:px-6 md:py-3 md:gap-3 md:justify-end lg:py-4 lg:px-8 lg:gap-5"
       style={{ zIndex: '999', ...fromUpSpring }}
     >
-      <button type="button" onClick={scrollToHome}>
+      <button
+        className="hover:underline underline-offset-8"
+        type="button"
+        onClick={scrollToHome}
+      >
         Home
       </button>
-      <Link to="/cv">CV</Link>
-      <button type="button" onClick={scrollToContact}>
+      <button
+        className="hover:underline underline-offset-8"
+        type="button"
+        onClick={scrollToContact}
+      >
         Contact
       </button>
     </animated.nav>
