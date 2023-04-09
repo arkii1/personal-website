08a7f79a42807e8367705f3cfda02399ed569a25
arkii1 <tommy.mapp@hotmail.com>
2023-04-09 13:23:53 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
+import Footer from './components/sections/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="personal-website" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
