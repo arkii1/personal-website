ba36c27631c6a0461efb4e5099583a0e472a2aab
arkii1 <tommy.mapp@hotmail.com>
2023-04-14 20:55:16 +0100

    Surround App in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import Footer from './components/sections/Footer'
 
 function App() {
   return (
-    <div className="App w-screen flex flex-col justify-start items-center overflow-x-hidden">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App w-screen flex flex-col justify-start items-center overflow-x-hidden">
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
